2aed93d3ba07db5c233e303fecf4c5d9e0cd6556
Bruce Belcher <50738384+BruceBelcher@users.noreply.github.com>
2021-02-16 15:42:09 +0000

    include build dir - removed from gitignore

---
+++ Coding/JavaScript/Calculator-made-with-React-master/build/precache-manifest.0c538549b8a33098ff8de48d66ec480e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa5b17f8675159eebd412b33499f938",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89ad72e8b4b5adb4e57a",
+    "url": "/static/css/main.a45cd45e.chunk.css"
+  },
+  {
+    "revision": "b000fccb79e89af31dc4",
+    "url": "/static/js/2.abf43ca4.chunk.js"
+  },
+  {
+    "revision": "89ad72e8b4b5adb4e57a",
+    "url": "/static/js/main.6adf6df3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
